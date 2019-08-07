fc3b961030e0bafb6b49e13557187da7e0404058
Alexandre Simard <alexandre@simard.click>
2019-08-07 15:24:14 -0400

    Tab now inserts suggestion into comment

--- js/main.js
+++ js/main.js
@@ -91,32 +91,48 @@
                     commenttextarea.append(suggestionspan);
                 }
             },
+            commentkeydown: function(event)
+            {
+                if(event.code == "Tab")
+                {
+                    event.preventDefault();
+                }
+            },
             generate: function(event)
             {
                 removeSuggestion();
 
                 var commenttextarea=document.getElementById('commenttext');
 
-                this.comment = commenttextarea.innerText
-
-                if(this.comment.replace(/\s/g, '').length)
+                if(event.code == "Tab")
                 {
-                    this.insertsuggestion();
+                    commenttextarea.innerText = this.comment = this.generatedtext;
+                    setEndOfContenteditable(commenttextarea);
+                    this.generatedtext = '';
                 }
-
-                if(event.code == "Space")
+                else
                 {
-                    postData('/api/generate',
-                    {text: this.comment})
-                    .then(data =>
-                        {
-                            if(!("error" in data))
+                    this.comment = commenttextarea.innerText;
+
+                    if(this.comment.replace(/\s/g, '').length)
+                    {
+                        this.insertsuggestion();
+                    }
+
+                    if(event.code == "Space")
+                    {
+                        postData('/api/generate',
+                        {text: this.comment})
+                        .then(data =>
                             {
-                                this.generatedtext = data.generated_text;
-                                this.insertsuggestion();
-                            }
-                        })
-                    .catch(error => console.error(error));
+                                if(!("error" in data))
+                                {
+                                    this.generatedtext = data.generated_text;
+                                    this.insertsuggestion();
+                                }
+                            })
+                        .catch(error => console.error(error));
+                    }
                 }
             }
         },
@@ -137,6 +153,27 @@
             .catch(error => console.error(error));
         }
     });
+
+    function setEndOfContenteditable(contentEditableElement)
+    {
+        var range,selection;
+        if(document.createRange)//Firefox, Chrome, Opera, Safari, IE 9+
+        {
+            range = document.createRange();//Create a range (a range is a like the selection but invisible)
+            range.selectNodeContents(contentEditableElement);//Select the entire contents of the element with the range
+            range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+            selection = window.getSelection();//get the selection object (allows you to change selection)
+            selection.removeAllRanges();//remove any selections already made
+            selection.addRange(range);//make the range you have just created the visible selection
+        }
+        else if(document.selection)//IE 8 and lower
+        { 
+            range = document.body.createTextRange();//Create a range (a range is a like the selection but invisible)
+            range.moveToElementText(contentEditableElement);//Select the entire contents of the element with the range
+            range.collapse(false);//collapse the range to the end point. false means collapse to end rather than the start
+            range.select();//Select the range (make it the visible selection
+        }
+    }
 
     function removeSuggestion()
     {
